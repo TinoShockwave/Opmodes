69c468867d2ebbe2161afd003e75337fb188762b
TheGiantPanda <kashyap.panda@gmail.com>
2015-11-18 15:28:12 -0800

    Gyro is fixed
    The MRGyroTest file is fixed. The gyro sensor can now be tested.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AutonomousExperimental.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AutonomousExperimental.java
@@ -3,6 +3,7 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 /**
  * Created by Kashyap Panda on 9/28/15.
 */
+
 public class AutonomousExperimental extends Autonomous {
     Autonomous robot;
 
